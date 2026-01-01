a1d63de7a35c991e5b41cd83e46317833001a40f
nguyenyou <tunt081295@gmail.com>
2026-01-01 10:04:19 +0700

    🚢 ship it

--- www/src/www/App.scala
+++ www/src/www/App.scala
@@ -6,22 +6,15 @@ import base.LaminarComponent
 case class App() extends LaminarComponent {
   def render(): HtmlElement = {
     div(
-      cls(
-        "min-h-screen w-screen flex flex-col justify-center items-center gap-8 py-8"
-      ),
+      cls("w-screen h-screen grid place-items-center"),
       div(
-        cls("text-center mb-4"),
-        h1(cls("text-2xl font-bold text-gray-800"), "DevTools Demo"),
-        p(
-          cls("text-gray-600"),
-          "Use these components to test the FPS, Memory, and MutationScanner"
-        )
-      ),
-      MutationDemo(),
-      div(
-        cls("flex flex-wrap gap-6 justify-center"),
-        FPSKiller(),
-        MemoryLeaker()
+        cls("flex gap-2"),
+        div(
+          cls("flex flex-col gap-2"),
+          FPSKiller(),
+          MemoryLeaker()
+        ),
+        MutationDemo()
       )
     )
   }
