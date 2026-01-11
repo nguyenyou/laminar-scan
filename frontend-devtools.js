2f8c009c53608c2baff9e3c66f51a2b3ec401598
nguyenyou <tunt081295@gmail.com>
2026-01-11 22:08:48 +0700

    🚢 ship it

--- frontend-devtools.js
+++ frontend-devtools.js
@@ -4488,7 +4488,7 @@
         border: 2px solid var(--fd-text-muted, #999);
         border-top: none;
         border-left: none;
-        border-bottom-right-radius: 4px;
+        border-bottom-right-radius: 8px;
         opacity: 0.5;
       }
 
